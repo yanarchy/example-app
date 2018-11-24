e6f53c8c86f36357d85e7ccc70bf5ac58b6d64f4
Yan Fan <theyanfan@gmail.com>
2018-11-24 09:14:38 +0900

    Fix

--- src/components/CurrentGif.jsx
+++ src/components/CurrentGif.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const CurrentGif = (props) => (
   <div className='current-gif-container'>
     <h2>{props.searchWords}</h2>
-    <img alt={`gif of $(props.searchWords}`} src={props.image} />
+    <img alt={`gif of ${props.searchWords}`} src={props.image} />
   </div>
 )
 
